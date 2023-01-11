c358ec77e0b8628b067b809b8c88a39d23a32b68
Sean Choi <szeanchoi@gmail.com>
2023-01-11 18:15:30 -0500

    Initial Commit

---
+++ IntroduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroduceStudent
+//
+//  Created by SEAN CHOI on 1/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
